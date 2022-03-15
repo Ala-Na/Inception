3ced53050e2af6d20253ca4d8ae0d6349b4d7402
anadege <anadege@student.42.fr>
2022-03-15 18:44:08 +0100

    non exiting maridb container

--- srcs/requirements/mariadb/tools/secure_installation.sh
+++ srcs/requirements/mariadb/tools/secure_installation.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# From https://bertvv.github.io/notes-to-self/2015/11/16/automating-mysql_secure_installation/
+# Mainly inspired from https://github.com/MariaDB/mariadb-docker/ official image 10.2
+# Adjustments principally from https://bertvv.github.io/notes-to-self/2015/11/16/automating-mysql_secure_installation/
+# and http://txt.fliglio.com/2013/11/creating-a-mysql-docker-container/
+
 
 set -o errexit # abort on nonzero existatus
 set -o nounset # abort on unbound variable
 
-is_mysql_command_available() {
-	which mysql > /dev/null 2>&1
-}
+# initialize database
+if [ ! -f /var/lib/mysql/mysql ]; then
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql --rpm > /dev/null
+fi
 
-if ! is_mysql_command_available; then
-	echo "The MySQL/MariaDB is not installed"
-
-mysql --user=root << _EOF_
+# secure installation
+# TODO fix password and maybe create database ?
+mysqld --user=mysql << _EOF_
 UPDATE mysql.user SET Password=PASSWORD('diffpasswordformysql') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+
 FLUSH PRIVILEGES;
 _EOF_
+
+exec /usr/bin/mysqld_safe --user=mysql
